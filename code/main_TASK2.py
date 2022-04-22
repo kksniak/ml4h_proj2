ec91ab0f8b6a778ed7116817ec62927ba46df4ac
kksniak <45125008+kksniak@users.noreply.github.com>
2022-04-22 11:14:18 +0200

    fix layout in main_TASK2.py

--- code/main_TASK2.py
+++ code/main_TASK2.py
@@ -31,21 +31,23 @@ embedding_creator = Embeddings("word2vec", sentences_train, sentences_val,
                                sentences_test)
 X_train, X_val, X_test = embedding_creator.train(load_model=True)
 
-### Fit Models (more models to be added)
+### Fit Models
 
-#### Vanilla
+#### word2vec with mean embedding
 word2vec_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=True)
+
+#### Vanilla
 model = Vanilla_NN("word2vec", word2vec_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
 y_pred = model.predict(X_test)
 evaluate("vanilla_nn_word2vec", y_pred, y_test, save_results=True)
 
-######################## ADD WORD2vec models here
 
-
+#### word2vec with concatenated embedding
 word2vec_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=False)
+
 #### BiLSTM 
 model = BiRNN_LSTM("word2vec", word2vec_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
@@ -53,6 +55,8 @@ y_pred = model.predict(X_test)
 evaluate("BiLSTM_word2vec", y_pred, y_test, save_results=True)
 
 #### BiLSTM with POS and position
+
+######## to be fixed
 main_dir = pathlib.Path(__file__).parents[1]
 pos_file_train = "pos_train"
 pos_file_val = "pos_val"
@@ -61,13 +65,16 @@ pos_file_test = "pos_test"
 pos = np.load(main_dir.joinpath(f"data/{pos_file_train}.npy"))
 pos_val = np.load(main_dir.joinpath(f"data/{pos_file_val}.npy"))
 pos_test = np.load(main_dir.joinpath(f"data/{pos_file_test}.npy"))
+##########
+
+
 model = BiRNN_LSTM_POS("word2vec", word2vec_embedding_layers,use_len_and_position= True)
 model.train(X_train, y_train, X_val, y_val,pos_train=pos, pos_val=pos_val,
             abstractPosFeat_train=abstractPosFeat_train,abstractPosFeat_val=abstractPosFeat_val, load_model=True)
 y_pred = model.predict(X_test, pos_test,abstractPosFeat_test)
 evaluate("BiLSTM_POS_word2vec", y_pred, y_test, save_results=True)
 
-#### ResNet1D
+#### ResNet1D with position
 model =ResNet1D_model("word2vec",word2vec_embedding_layers,use_len_and_position = True)
 model.train(X_train,y_train, X_val, y_val,abstractPosFeat_train=abstractPosFeat_train,
             abstractPosFeat_val=abstractPosFeat_val, load_model=False)
@@ -83,17 +90,19 @@ embedding_creator = Embeddings("fastText", sentences_train, sentences_val,
                                sentences_test)
 X_train, X_val, X_test = embedding_creator.train(load_model=True)
 
-### Fit Models (more models to be added)
+### Fit Models
 
-#### Vanilla NN
+#### FastText with mean embedding
 fasttext_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=True)
+
+#### Vanilla NN
 model = Vanilla_NN("fastText", fasttext_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
 y_pred = model.predict(X_test)
 evaluate("vanilla_nn_fastText", y_pred, y_test, save_results=True)
 
-######################## ADD fasttext models here
+#### FastText with concatenated embedding
 fasttext_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=False)
 
@@ -104,14 +113,13 @@ y_pred = model.predict(X_test)
 evaluate("BiLSTM_fastText", y_pred, y_test, save_results=True)
 
 #### BiLSTM with POS and position
-
 model = BiRNN_LSTM_POS("fastText", fasttext_embedding_layers,use_len_and_position= True)
 model.train(X_train, y_train, X_val, y_val,pos_train=pos, pos_val=pos_val,
             abstractPosFeat_train=abstractPosFeat_train,abstractPosFeat_val=abstractPosFeat_val, load_model=False)
 y_pred = model.predict(X_test, pos_test,abstractPosFeat_test)
 evaluate("BiLSTM_POS_fastText", y_pred, y_test, save_results=True)
 
-#### ResNet1D
+#### ResNet1D with position
 model =ResNet1D_model("fastText",fasttext_embedding_layers,use_len_and_position = True)
 model.train(X_train,y_train, X_val, y_val,abstractPosFeat_train=abstractPosFeat_train,
             abstractPosFeat_val=abstractPosFeat_val, load_model=True)
@@ -119,27 +127,29 @@ y_pred = model.predict(X_test,abstractPosFeat_test)
 evaluate("resnet1d_position_fastText", y_pred, y_test, save_results=True)
 
 
-## Tokeniser + trainable keras embedding Layer only
+## Trainable keras embedding Layer
 
 ### Create Embedding
 embedding_creator = Embeddings("kerasEmbed", sentences_train, sentences_val,
                                sentences_test)
 X_train, X_val, X_test = embedding_creator.train()
 
-### Fit Models (more models to be added)
+### Fit Models
 
-#### Vanilla NN
+#### Keras Embedding Layer with mean embedding
 keras_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=True)
+
+#### Vanilla NN
 model = Vanilla_NN("kerasEmbed", keras_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
 y_pred = model.predict(X_test)
 evaluate("vanilla_nn_kerasEmbed", y_pred, y_test, save_results=True)
 
-######################## ADD keras embedding models here
-
+#### Keras Layer embedding with concatenated embedding
 keras_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=False)
+
 #### BiLSTM 
 model = BiRNN_LSTM("kerasEmbed", keras_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
@@ -147,14 +157,13 @@ y_pred = model.predict(X_test)
 evaluate("BiLSTM_kerasEmbed", y_pred, y_test, save_results=True)
 
 #### BiLSTM with POS and position
-
 model = BiRNN_LSTM_POS("kerasEmbed", keras_embedding_layers,use_len_and_position= True)
 model.train(X_train, y_train, X_val, y_val,pos_train=pos, pos_val=pos_val,
             abstractPosFeat_train=abstractPosFeat_train,abstractPosFeat_val=abstractPosFeat_val, load_model=True)
 y_pred = model.predict(X_test, pos_test,abstractPosFeat_test)
 evaluate("BiLSTM_POS_kerasEmbed", y_pred, y_test, save_results=True)
 
-#### ResNet1D
+#### ResNet1D with position
 model =ResNet1D_model("kerasEmbed",keras_embedding_layers,use_len_and_position = True)
 model.train(X_train,y_train, X_val, y_val,abstractPosFeat_train=abstractPosFeat_train,
             abstractPosFeat_val=abstractPosFeat_val, load_model=True)
