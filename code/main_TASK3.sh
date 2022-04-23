f8e46a176027cbaebf944904c7395c82914eb7be
Frithiof Ekström <frithiof.ekstrom@gmail.com>
2022-04-23 12:32:30 +0200

    Update task 3 main script

--- code/main_TASK3.sh
+++ code/main_TASK3.sh
@@ -1,19 +1,26 @@
+# Run this from the `code` directory
 
 # Create datasets
 echo 'Preparing datasets...'
 python utils.py
 
 # Load models and evaluate them on a small test set
-python -m models.bert --params results/Bio_ClinicalBERT_untuned/params.txt --checkpoint results/Bio_ClinicalBERT_untuned/checkpoint/checkpoint --dataset debug --skip-training
-python -m models.bert --params results/Bio_ClinicalBERT_tuned/params.txt --checkpoint results/Bio_ClinicalBERT_tuned/checkpoint/checkpoint --dataset debug --skip-training
-python -m models.bert --params results/SapBERT_tuned/params.txt --checkpoint results/SapBERT_tuned/checkpoint/checkpoint --dataset debug --skip-training
+echo 'Evaluating models on debug test set...'
+python -m models.bert --params ../results/Bio_ClinicalBERT_untuned/params.txt --checkpoint models/models_checkpoints/Bio_ClinicalBERT_untuned/checkpoint --dataset debug --skip-training
+python -m models.bert --params ../results/Bio_ClinicalBERT_tuned/params.txt --checkpoint models/models_checkpoints/Bio_ClinicalBERT_tuned/checkpoint --dataset debug --skip-training
+python -m models.bert --params ../results/SapBERT_tuned/params.txt --checkpoint models/models_checkpoints/SapBERT_tuned/checkpoint --dataset debug --skip-training
+python -m models.bert --params ../results/SapBERT_untuned/params.txt --checkpoint models/models_checkpoints/SapBERT_untuned/checkpoint --dataset debug --skip-training
 
 # Uncomment to evaluate on the full test set (takes a few hours on CPU, a few minutes on GPU)
-# python -m models.bert --params results/Bio_ClinicalBERT_untuned/params.txt --checkpoint results/Bio_ClinicalBERT_untuned/checkpoint/checkpoint --dataset small_balanced --skip-training
-# python -m models.bert --params results/Bio_ClinicalBERT_tuned/params.txt --checkpoint results/Bio_ClinicalBERT_tuned/checkpoint/checkpoint --dataset small_balanced --skip-training
-# python -m models.bert --params results/SapBERT_tuned/params.txt --checkpoint results/SapBERT_tuned/checkpoint/checkpoint --dataset small_balanced --skip-training
+# echo 'Evaluating models on full test set...'
+# python -m models.bert --params ../results/Bio_ClinicalBERT_untuned/params.txt --checkpoint models/models_checkpoints/Bio_ClinicalBERT_untuned/checkpoint --dataset small_balanced --skip-training
+# python -m models.bert --params ../results/Bio_ClinicalBERT_tuned/params.txt --checkpoint models/models_checkpoints/Bio_ClinicalBERT_tuned/checkpoint --dataset small_balanced --skip-training
+# python -m models.bert --params ../results/SapBERT_tuned/params.txt --checkpoint models/models_checkpoints/SapBERT_tuned/checkpoint --dataset small_balanced --skip-training
+# python -m models.bert --params ../results/SapBERT_untuned/params.txt --checkpoint models/models_checkpoints/SapBERT_untuned/checkpoint --dataset small_balanced --skip-training
 
 # Uncomment to train models from scratch and evaluate (takes a few hours on GPU)
-# python -m models.bert --params results/Bio_ClinicalBERT_tuned/params.txt
-# python -m models.bert --params results/Bio_ClinicalBERT_untuned/params.txt
-# python -m models.bert --params results/SapBERT_tuned/params.txt
+# echo 'Training models from scratch...'
+# python -m models.bert --params ../results/Bio_ClinicalBERT_untuned/params.txt
+# python -m models.bert --params ../results/Bio_ClinicalBERT_tuned/params.txt
+# python -m models.bert --params ../results/SapBERT_tuned/params.txt
+# python -m models.bert --params ../results/SapBERT_untuned/params.txt
