ab4cfc85afcfe74dc73179c6493549a3c6bb7eba
Frithiof Ekström <frithiof.ekstrom@gmail.com>
2022-04-26 01:36:44 +0200

    Load all models

--- code/main_TASK2.py
+++ code/main_TASK2.py
@@ -43,35 +43,47 @@ model.train(X_train, y_train, X_val, y_val, load_model=True)
 y_pred = model.predict(X_test)
 evaluate("vanilla_nn_word2vec", y_pred, y_test, save_results=True)
 
-
 #### word2vec with concatenated embedding
 word2vec_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=False)
 
-#### BiLSTM 
+#### BiLSTM
 model = BiRNN_LSTM("word2vec", word2vec_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
 y_pred = model.predict(X_test)
 evaluate("BiLSTM_word2vec", y_pred, y_test, save_results=True)
 
+pos, pos_val, pos_test = get_POS_encoding(sentences_train, sentences_val,
+                                          sentences_test, True)
 
-pos, pos_val, pos_test = get_POS_encoding(sentences_train,sentences_val,sentences_test,True)
-
-
-model = BiRNN_LSTM_POS("word2vec", word2vec_embedding_layers,use_len_and_position= True)
-model.train(X_train, y_train, X_val, y_val,pos_train=pos, pos_val=pos_val,
-            abstractPosFeat_train=abstractPosFeat_train,abstractPosFeat_val=abstractPosFeat_val, load_model=True)
-y_pred = model.predict(X_test, pos_test,abstractPosFeat_test)
+model = BiRNN_LSTM_POS("word2vec",
+                       word2vec_embedding_layers,
+                       use_len_and_position=True)
+model.train(X_train,
+            y_train,
+            X_val,
+            y_val,
+            pos_train=pos,
+            pos_val=pos_val,
+            abstractPosFeat_train=abstractPosFeat_train,
+            abstractPosFeat_val=abstractPosFeat_val,
+            load_model=True)
+y_pred = model.predict(X_test, pos_test, abstractPosFeat_test)
 evaluate("BiLSTM_POS_word2vec", y_pred, y_test, save_results=True)
 
 #### ResNet1D with position
-model =ResNet1D_model("word2vec",word2vec_embedding_layers,use_len_and_position = True)
-model.train(X_train,y_train, X_val, y_val,abstractPosFeat_train=abstractPosFeat_train,
-            abstractPosFeat_val=abstractPosFeat_val, load_model=False)
-y_pred = model.predict(X_test,abstractPosFeat_test)
+model = ResNet1D_model("word2vec",
+                       word2vec_embedding_layers,
+                       use_len_and_position=True)
+model.train(X_train,
+            y_train,
+            X_val,
+            y_val,
+            abstractPosFeat_train=abstractPosFeat_train,
+            abstractPosFeat_val=abstractPosFeat_val,
+            load_model=True)
+y_pred = model.predict(X_test, abstractPosFeat_test)
 evaluate("resnet1d_position_word2vec", y_pred, y_test, save_results=True)
-
-
 
 ## FastText
 
@@ -96,26 +108,41 @@ evaluate("vanilla_nn_fastText", y_pred, y_test, save_results=True)
 fasttext_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=False)
 
-#### BiLSTM 
+#### BiLSTM
 model = BiRNN_LSTM("fastText", fasttext_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
 y_pred = model.predict(X_test)
 evaluate("BiLSTM_fastText", y_pred, y_test, save_results=True)
 
 #### BiLSTM with POS and position
-model = BiRNN_LSTM_POS("fastText", fasttext_embedding_layers,use_len_and_position= True)
-model.train(X_train, y_train, X_val, y_val,pos_train=pos, pos_val=pos_val,
-            abstractPosFeat_train=abstractPosFeat_train,abstractPosFeat_val=abstractPosFeat_val, load_model=False)
-y_pred = model.predict(X_test, pos_test,abstractPosFeat_test)
+model = BiRNN_LSTM_POS("fastText",
+                       fasttext_embedding_layers,
+                       use_len_and_position=True)
+model.train(X_train,
+            y_train,
+            X_val,
+            y_val,
+            pos_train=pos,
+            pos_val=pos_val,
+            abstractPosFeat_train=abstractPosFeat_train,
+            abstractPosFeat_val=abstractPosFeat_val,
+            load_model=True)
+y_pred = model.predict(X_test, pos_test, abstractPosFeat_test)
 evaluate("BiLSTM_POS_fastText", y_pred, y_test, save_results=True)
 
 #### ResNet1D with position
-model =ResNet1D_model("fastText",fasttext_embedding_layers,use_len_and_position = True)
-model.train(X_train,y_train, X_val, y_val,abstractPosFeat_train=abstractPosFeat_train,
-            abstractPosFeat_val=abstractPosFeat_val, load_model=True)
-y_pred = model.predict(X_test,abstractPosFeat_test)
+model = ResNet1D_model("fastText",
+                       fasttext_embedding_layers,
+                       use_len_and_position=True)
+model.train(X_train,
+            y_train,
+            X_val,
+            y_val,
+            abstractPosFeat_train=abstractPosFeat_train,
+            abstractPosFeat_val=abstractPosFeat_val,
+            load_model=True)
+y_pred = model.predict(X_test, abstractPosFeat_test)
 evaluate("resnet1d_position_fastText", y_pred, y_test, save_results=True)
-
 
 ## Trainable keras embedding Layer
 
@@ -140,23 +167,38 @@ evaluate("vanilla_nn_kerasEmbed", y_pred, y_test, save_results=True)
 keras_embedding_layers = embedding_creator.get_embedding_layer(
     mean_embedding=False)
 
-#### BiLSTM 
+#### BiLSTM
 model = BiRNN_LSTM("kerasEmbed", keras_embedding_layers)
 model.train(X_train, y_train, X_val, y_val, load_model=True)
 y_pred = model.predict(X_test)
 evaluate("BiLSTM_kerasEmbed", y_pred, y_test, save_results=True)
 
 #### BiLSTM with POS and position
-model = BiRNN_LSTM_POS("kerasEmbed", keras_embedding_layers,use_len_and_position= True)
-model.train(X_train, y_train, X_val, y_val,pos_train=pos, pos_val=pos_val,
-            abstractPosFeat_train=abstractPosFeat_train,abstractPosFeat_val=abstractPosFeat_val, load_model=True)
-y_pred = model.predict(X_test, pos_test,abstractPosFeat_test)
+model = BiRNN_LSTM_POS("kerasEmbed",
+                       keras_embedding_layers,
+                       use_len_and_position=True)
+model.train(X_train,
+            y_train,
+            X_val,
+            y_val,
+            pos_train=pos,
+            pos_val=pos_val,
+            abstractPosFeat_train=abstractPosFeat_train,
+            abstractPosFeat_val=abstractPosFeat_val,
+            load_model=True)
+y_pred = model.predict(X_test, pos_test, abstractPosFeat_test)
 evaluate("BiLSTM_POS_kerasEmbed", y_pred, y_test, save_results=True)
 
 #### ResNet1D with position
-model =ResNet1D_model("kerasEmbed",keras_embedding_layers,use_len_and_position = True)
-model.train(X_train,y_train, X_val, y_val,abstractPosFeat_train=abstractPosFeat_train,
-            abstractPosFeat_val=abstractPosFeat_val, load_model=True)
-y_pred = model.predict(X_test,abstractPosFeat_test)
+model = ResNet1D_model("kerasEmbed",
+                       keras_embedding_layers,
+                       use_len_and_position=True)
+model.train(X_train,
+            y_train,
+            X_val,
+            y_val,
+            abstractPosFeat_train=abstractPosFeat_train,
+            abstractPosFeat_val=abstractPosFeat_val,
+            load_model=True)
+y_pred = model.predict(X_test, abstractPosFeat_test)
 evaluate("resnet1d_position_kerasEmbed", y_pred, y_test, save_results=True)
-
